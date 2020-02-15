497bfa8bdaa57712771908ec1b26d09ec2b3b030
zx <929535000@qq.com>
2020-02-15 15:23:28 +0800

    导航栏

--- src/main/java/life/majiang/community/controller/Hello.java
+++ src/main/java/life/majiang/community/controller/Hello.java
@@ -1,15 +1,12 @@
 package life.majiang.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Hello {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
+    @GetMapping("/")
+    public String hello(){
         return "hello";
     }
 }
